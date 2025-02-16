d4ea0f058a1b7e1039aac2f6b9dad94a34b713e6
imsein1005 <vicent77@iabacus.co.kr>
2025-02-16 19:37:49 +0900

    feature : api 어댑터

--- src/main.js
+++ src/main.js
@@ -5,11 +5,19 @@ import { createApp } from 'vue'
 import pinia from '@/stores'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
+//import { openErrorPopUp } from '@/core/utils/errorPopup'
+import RestApiAdapter from '@/adapters/restApiAdapter'
 
 const app = createApp(App)
 
 app.use(pinia)
 app.use(router)
 app.use(vuetify)
+
+// app.config.errorHandler = async (err, vm, info) => {
+//     openErrorPopUp(err, vm, info)
+// }
+
+RestApiAdapter.initialize(app._context)
 
 app.mount('#app')
