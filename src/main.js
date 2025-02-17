98add56b131ed7a21443f30fcde6dc26fcab80d2
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-17 14:07:55 +0900

    feature: 헤더 컴포넌트 생성 및 기본 페이지 추가

--- src/main.js
+++ src/main.js
@@ -5,9 +5,12 @@ import { createApp } from 'vue'
 import pinia from '@/stores'
 import router from '@/router'
 import vuetify from '@/plugins/vuetify'
+import { registerLayouts } from './layouts/register';
+
 
 const app = createApp(App)
 
+registerLayouts(app);
 app.use(pinia)
 app.use(router)
 app.use(vuetify)
--- src/router/index.js
+++ src/router/index.js
@@ -1,23 +1,70 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import HomeView from '@/views/HomeView.vue'
+
+import HomeView from '@/views/HomePage.vue'
 import TableTestPage from '@/views/TableTestPage.vue'
+import ProjectPage from '@/views/ProjectPage.vue'
+import DefaultLayout from '@/layouts/DefaultLayout.vue'
+import MemberPage from '@/views/MemberPage.vue'
+import PartnersPage from '@/views/PartnersPage.vue'
+import PermissionPage from '@/views/PermissionPage.vue'
+import SalesPage from '@/views/SalesPage.vue'
+import ProfilePage from '@/views/ProfilePage.vue'
+
+const routes = [
+  {
+    path: '/',
+    name: 'home',
+    component: HomeView,
+    meta: { layout: DefaultLayout },
+  },
+  {
+    path: '/projects',
+    name: 'projects',
+    component: ProjectPage,
+    meta: { layout: DefaultLayout, activeIndex: 0 },
+  },
+  {
+    path: '/members',
+    name: 'members',
+    component: MemberPage,
+    meta: { layout: DefaultLayout, activeIndex: 1 },
+  },
+  {
+    path: '/partners',
+    name: 'partners',
+    component: PartnersPage,
+    meta: { layout: DefaultLayout, activeIndex: 2 },
+  },
+  {
+    path: '/sales',
+    name: 'sales',
+    component: SalesPage,
+    meta: { layout: DefaultLayout, activeIndex: 3 },
+  },
+  {
+    path: '/permissions',
+    name: 'permissions',
+    component: PermissionPage,
+    meta: { layout: DefaultLayout, activeIndex: 4 },
+  },
+  {
+    path: '/profiles',
+    name: 'profiles',
+    component: ProfilePage,
+    meta: { layout: DefaultLayout, activeIndex: 5 },
+  },
+  /* [추후 삭제 예정] 테이블 컴포넌트 테스트용으로 만든 페이지입니다. */
+  {
+    path: '/table',
+    name: 'table',
+    component: TableTestPage,
+    meta: { layout: DefaultLayout },
+  },
+]
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: [
-    {
-      path: '/',
-      name: 'home',
-      component: HomeView,
-    },
-
-    /* [추후 삭제 예정] 테이블 컴포넌트 테스트용으로 만든 페이지입니다. */
-    {
-      path: '/table',
-      name: 'table',
-      component: TableTestPage,
-    },
-  ],
+  routes,
 })
 
 export default router
