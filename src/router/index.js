144cb5626db2a9ddcc203ae212dc1fd97fccbc89
이동욱 <ledu202@gmail.com>
2025-02-18 09:16:57 +0900

    feature: 검색 바 컴포넌트 테스트 uri 추가

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import HomeView from '@/views/HomeView.vue'
 import TableTestPage from '@/views/TableTestPage.vue'
+import SearchBarTestPage from '@/views/SearchBarTestPage.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -17,6 +18,11 @@ const router = createRouter({
       name: 'table',
       component: TableTestPage,
     },
+    {
+      path: '/searchbar',
+      name: 'searchbar',
+      component: SearchBarTestPage
+    }
   ],
 })
 
