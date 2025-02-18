da9c2f7c1f2daa6ba892bc26c1468c146b678845
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-18 21:36:00 +0900

    feature: api 예시 구현 및 gitignore 변경

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -2,9 +2,13 @@ import { createVuetify } from 'vuetify'
 
 import '@mdi/font/css/materialdesignicons.css'
 import 'vuetify/styles'
+import { VTreeview } from 'vuetify/labs/VTreeview'
 
 export default createVuetify({
   theme: {
     defaultTheme: 'light',
   },
+  components: {
+    VTreeview,
+  }
 })
