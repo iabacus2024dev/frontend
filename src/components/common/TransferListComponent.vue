3f6b0a222d9eb71211d27c42a68feee92ad7e4d3
Hajin-Abacus <qwertyv@iabacus.co.kr>
2025-02-19 15:10:06 +0900

    fix: 트랜스퍼리스트 컴포넌트 스타일 수정

--- src/components/common/TransferListComponent.vue
+++ src/components/common/TransferListComponent.vue
@@ -1,84 +1,95 @@
 <template>
-  <v-card>
-    <v-toolbar>
-      <template v-slot:prepend>
-        <v-toolbar-title>조직원 선택</v-toolbar-title>
-      </template>
-    </v-toolbar>
+<v-card>
+  <!-- 헤더 -->
+  <v-toolbar class="toolbar-header">
+    <v-toolbar-title>조직원 선택</v-toolbar-title>
+  </v-toolbar>
 
-    <v-row>
-      <v-col>
-        <v-text-field
-          v-model="search"
-          clear-icon="mdi-close-circle-outline"
-          label="조직원을 검색하세요."
-          variant="solo-filled">
-        </v-text-field>
-        <v-card-text>
-          <v-treeview
-            v-model:selected="tree"
-            :items="items"
-            :load-children="load"
-            item-title="name"
-            item-value="id"
-            select-strategy="leaf"
-            return-object
-            selectable
-            :search="search"
-          ></v-treeview>
-        </v-card-text>
-      </v-col>
+  <!-- 트랜스퍼 리스트 -->
+  <v-row>
 
-      <v-divider
-        :vertical="$vuetify.display.mdAndUp"
-        class="my-md-3">
-      </v-divider>
+    <!-- 왼쪽 -->
+    <v-col cols="5" class="left">
+      <!-- 검색 바 -->
+      <v-text-field
+        class="search-bar"
+        v-model="search"
+        label="조직원을 검색하세요."
+        variant="outlined"
+        density="compact" />
 
-      <v-col cols="12" md="6">
-        <v-card-text>
-          <div class="text-h6 font-weight-light text-grey pa-4 text-center">
-            선택된 조직원: {{ selectedCount }} / 전체 조직원: {{ totalCount }}
-          </div>
-        </v-card-text>
+      <!-- 트리 뷰 -->
+      <v-treeview
+        v-model:selected="tree"
+        :items="items"
+        :load-children="load"
+        item-title="name"
+        item-value="id"
+        select-strategy="classic"
+        return-object
+        selectable
+        :search="search"
+        density="compact" />
+    </v-col>
 
-        <v-card-text>
-          <div
+    <v-divider :vertical="$vuetify.display.mdAndUp"/>
+
+    <!-- 오른쪽 -->
+    <v-col class="right">
+
+        <!-- 헤더 -->
+        <div class="selected-item-header">
+          선택 ({{ selectedCount }} / {{ totalCount }})
+        </div>
+
+        <v-divider />
+
+        <!-- 선택 칩 -->
+        <div class="chip-wrapper">
+          <span
             v-if="tree.length === 0"
-            key="title"
-            class="text-h6 font-weight-light text-grey pa-4 text-center">
+            key="title">
             선택된 조직원이 없습니다.
-          </div>
+          </span>
 
-          <v-scroll-x-transition group hide-on-leave>
-            <v-chip
-              v-for="(selection, i) in tree"
-              :key="i"
-              :text="selection.name"
-              class="ma-1"
-              color="grey"
-              size="small">
-            </v-chip>
-          </v-scroll-x-transition>
-        </v-card-text>
-      </v-col>
-    </v-row>
+          <v-chip
+            v-for="(selection, i) in tree"
+            :key="i"
+            :text="selection.name"
+            class="ma-1"
+            color="grey"
+            size="small">
+          </v-chip>
+        </div>
+    </v-col>
+  </v-row>
 
-    <v-divider></v-divider>
+  <v-divider />
 
-    <v-card-actions>
-      <v-btn text="초기화" variant="text" @click="tree = []" class="reset-btn"></v-btn>
-      <v-spacer></v-spacer>
-      <v-btn
-        append-icon="mdi-content-save"
-        text="저장"
-        class="save-btn">
-      </v-btn>
-    </v-card-actions>
-  </v-card>
+  <!-- 버튼 -->
+  <v-card-actions>
+    <v-btn
+      text="초기화"
+      variant="text"
+      @click="tree = []"
+      class="reset-btn">
+    </v-btn>
+    <v-btn
+      text="적용"
+      class="save-btn">
+    </v-btn>
+  </v-card-actions>
+</v-card>
 </template>
 
 <script setup>
-import { ref, watch, computed, onMounted } from 'vue'
+import { ref, computed, onMounted } from 'vue'
+import { defineProps } from 'vue';
+
+const props = defineProps({
+  rawDataProps: Object,
+  test: Array,
+})
 
 const tree = ref([])
 const search = ref(null)
@@ -107,8 +118,7 @@ const rawData = ref(
       "지능플랫폼사업팀": ["금정우 팀장", "김도연 책임"],
     }
   },
-}
-);
+});
 
 /* 리스트를 트리뷰 구조로 바꾸는 함수 */
 const transformToTree = (data, parentId = 1) => {
@@ -131,6 +141,7 @@ const transformToTree = (data, parentId = 1) => {
 
 onMounted(() => {
   items.value = transformToTree(rawData.value);
+  console.log(props.test);
 });
 
 
@@ -145,17 +156,80 @@ function load() {
 </script>
 
 <style scoped>
-.v-toolbar {
+.v-card {
+  width: 800px;
+  height: 550px;
+  display: flex;
+  flex-direction: column;
+}
+
+/* 헤더 */
+.toolbar-header {
+  height: 50px;
+  display: flex;
+  flex-direction: row;
+  align-items: center;
   background-color: #EB6129;
+}
+
+.toolbar-header .v-toolbar-title {
+  font-size: 20px;
   color: white;
 }
 
-.v-toolbar-title {
-  padding: 0 10px;
+/* 메인 (왼쪽) */
+.v-row {
+  padding: 0;
+  margin: 0;
 }
 
-.v-text-field {
-  margin: 20px 20px 0px 20px;
+.v-col.left {
+  padding: 15px;
+  margin: 0;
+}
+
+.v-col.right {
+  padding: 0px;
+  margin: 0px;
+}
+
+.search-bar {
+  width: 100%;
+  height: 40px;
+}
+
+.v-treeview {
+  margin-top: 15px;
+  height: 360px;
+  overflow-y: auto;
+  border: 1px solid #ddd;
+  border-radius: 5px;
+}
+
+/* 메인 (오른쪽) */
+.selected-item-header {
+  text-align: end;
+  color: lightgrey;
+  padding: 10px;
+}
+
+.chip-wrapper {
+  padding: 10px 20px;
+}
+
+.chip-wrapper span {
+  color: lightgray;
+}
+
+/* 버튼 */
+.v-card-actions {
+  height: 40px;
+  display: flex;
+  justify-content: end;
+}
+
+.v-card-actions .v-btn {
+  height: 30px;
 }
 
 .save-btn {
@@ -164,6 +238,7 @@ function load() {
 }
 
 .reset-btn {
-  background-color: rgb(241, 241, 241);
+  background-color: lightgrey;
 }
+
 </style>
