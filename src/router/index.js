4b7c980308cad6cb2c74fe17b254dced6c925d46
YUUY-YUUY <55330m@iabacus.co.kr>
2025-02-20 13:11:11 +0900

    Update index.js
    index.js 수정

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 
-import HomeView from '@/views/HomePage.vue'
+import HomePage from '@/views/HomePage.vue'
 import TableTestPage from '@/views/TableTestPage.vue'
 import SearchBarTestPage from '@/views/SearchBarTestPage.vue'
 import ProjectPage from '@/views/ProjectPage.vue'
@@ -13,21 +13,6 @@ import ProfilePage from '@/views/ProfilePage.vue'
 import ProgressBarComponent from '@/components/common/ProgressBarComponent.vue'
 import TreeTestPage from '@/views/TreeTestPage.vue'
 import ToolTipTestPage from '@/views/ToolTipTestPage.vue'
-
-import HomePage from '@/views/HomePage.vue';
-import TableTestPage from '@/views/TableTestPage.vue';
-import SearchBarTestPage from '@/views/SearchBarTestPage.vue';
-import ProjectPage from '@/views/ProjectPage.vue';
-import DefaultLayout from '@/layouts/DefaultLayout.vue';
-import MemberPage from '@/views/MemberPage.vue';
-import PartnersPage from '@/views/PartnersPage.vue';
-import PermissionPage from '@/views/PermissionPage.vue';
-import SalesPage from '@/views/SalesPage.vue';
-import ProfilePage from '@/views/ProfilePage.vue';
-import ToolTipComponent from '@/components/common/ToolTipComponent.vue';
-import ProgressBarComponent from '@/components/common/ProgressBarComponent.vue';
-import TransferListTestPage from '@/views/TransferListTestPage.vue';
-import TreeTestPage from '@/views/TreeTestPage.vue';
 
 const routes = [
   {
