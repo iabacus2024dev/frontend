75f820f20c348db3fca456f258141cb190b33cae
lamine-yamal19 <jingyu10@iabacus.co.kr>
2025-02-21 17:56:21 +0900

    feature: 로그인

---
+++ src/components/login/LoginComponent.vue
@@ -0,0 +1,97 @@
+<script setup>
+import {ref} from "vue";
+
+const form = ref(false);
+const email = ref(null);
+const loading = ref(false);
+const password = ref(null);
+const visible = ref(false);
+
+const onSubmit = () => {
+  console.log("submit")
+  if (!this.form) {
+    return;
+  }
+
+  this.loading = true;
+  setTimeout(() => {
+    this.loading = false
+  }, 2000);
+}
+const required = (v) => {
+  return !!v || '입력값이 비었습니다';
+}
+</script>
+
+<template>
+  <v-sheet class="d-flex align-center justify-center" height="70vh">
+    <v-card class="px-6 py-8" elevation="0" width="380">
+      <v-img
+        class="mx-auto my-12"
+        max-width="270"
+        style="cursor: pointer"
+        @click="goTo('/')"
+        src="http://www.iabacus.co.kr/data/designImages/BANNER_1665234808abacus_logo.png"
+      />
+      <v-form
+        v-model="form"
+        @submit.prevent="onSubmit"
+      >
+
+        <v-text-field
+          v-model="email"
+          :readonly="loading"
+          :rules="[required]"
+          label="이메일"
+          class="mb-2"
+          prepend-inner-icon="mdi-email-outline"
+          variant="outlined"
+          hide-details="auto"
+        ></v-text-field>
+
+        <v-text-field
+          v-model="password"
+          :readonly="loading"
+          :rules="[required]"
+          :append-inner-icon="visible ? 'mdi-eye-off' : 'mdi-eye'"
+          :type="visible ? 'text' : 'password'"
+          prepend-inner-icon="mdi-lock-outline"
+          variant="outlined"
+          @click:append-inner="visible = !visible"
+          label="비밀번호"
+          placeholder="비밀번호를 입력하세요"
+          clearable
+          clearable-icon="mdi-close"
+          hide-details="auto"
+        ></v-text-field>
+
+        <br>
+
+        <v-btn
+          :loading="loading"
+          color="#EB6129"
+          size="large"
+          type="submit"
+          variant="elevated"
+          rounded="lg"
+          block
+        >
+          로그인
+        </v-btn>
+      </v-form>
+      <v-row justify="center" class="mt-2 text-body-2">
+        <v-col cols="auto">
+          <a href="/register" class="text-decoration-none text-grey-darken-1">회원가입</a>
+        </v-col>
+        <v-col cols="auto" class="px-2">|</v-col>
+        <v-col cols="auto">
+          <a href="/password-reset" class="text-decoration-none text-grey-darken-1">비밀번호 찾기</a>
+        </v-col>
+      </v-row>
+    </v-card>
+  </v-sheet>
+</template>
+
+<style scoped>
+
+</style>
---
+++ src/views/LoginPage.vue
@@ -0,0 +1,12 @@
+<script setup>
+
+import LoginComponent from "@/components/login/LoginComponent.vue";
+</script>
+
+<template>
+  <LoginComponent />
+</template>
+
+<style scoped>
+
+</style>
