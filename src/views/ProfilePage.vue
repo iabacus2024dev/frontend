e17030f827f1ac2fcb03317875badcb4181b3bb3
sangcheol <tkdcjf38@iabacus.co.kr>
2025-02-24 22:05:45 +0900

    fix: ProfilePage 수정

--- src/views/ProfilePage.vue
+++ src/views/ProfilePage.vue
@@ -1,24 +1,24 @@
 <template>
-  <v-row>
-    <!-- Sidebar Profile Card -->
-    <v-col cols="12" md="4">
-      <BasicProfileComponent />
-      <PasswordChangeComponent />
-      <DeleteMemberComponent />
-    </v-col>
-    <v-col cols="12" md="8">
-      <ProjectListComponent />
-      <TeamListComponent />
-    </v-col>
-  </v-row>
+  <!--  <v-row>-->
+  <!--    &lt;!&ndash; Sidebar Profile Card &ndash;&gt;-->
+  <!--    <v-col cols="12" md="4">-->
+  <!--      <BasicProfileComponent />-->
+  <!--      <PasswordChangeComponent />-->
+  <!--      <DeleteMemberComponent />-->
+  <!--    </v-col>-->
+  <!--    <v-col cols="12" md="8">-->
+  <!--      <ProjectListComponent />-->
+  <!--      <TeamListComponent />-->
+  <!--    </v-col>-->
+  <!--  </v-row>-->
 </template>
 
 <script setup>
-import BasicProfileComponent from '@/components/profile/BasicProfileComponent.vue'
-import ProjectListComponent from '@/components/profile/ProjectListComponent.vue'
-import PasswordChangeComponent from '@/components/profile/PasswordChangeComponent.vue'
-import TeamListComponent from '@/components/profile/TeamListComponent.vue'
-import DeleteMemberComponent from '@/components/profile/DeleteMemberComponent.vue'
+// import BasicProfileComponent from '@/components/profile/BasicProfileComponent.vue'
+// import ProjectListComponent from '@/components/profile/ProjectListComponent.vue'
+// import PasswordChangeComponent from '@/components/profile/PasswordChangeComponent.vue'
+// import TeamListComponent from '@/components/profile/TeamListComponent.vue'
+// import DeleteMemberComponent from '@/components/profile/DeleteMemberComponent.vue'
 </script>
 
 <style scoped></style>
