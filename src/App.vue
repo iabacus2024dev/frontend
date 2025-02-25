1679265aa3ffce3adb6ef61978be53c8a588ce04
Hajin-Abacus <qwertyv@iabacus.co.kr>
2025-02-25 16:28:18 +0900

    feature: 다이얼로그 컴포넌트 렌더링

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,30 @@
 <script setup>
 import { RouterView } from 'vue-router'
+import DialogComponent from './components/common/DialogComponent.vue'
+import { useDialog } from './composables/useDialog'
+
+const { dialogs, closeDialog } = useDialog()
+
+console.log('dialogs >>>', dialogs)
+
+const handleDialogCancelBtn = (dialogId) => {
+  console.log('dialogs >>>', dialogId)
+  closeDialog(dialogId)
+}
 </script>
 
 <template>
   <component :is="$route.meta.layout || 'div'">
     <RouterView />
   </component>
+
+  <DialogComponent />
+  <DialogComponent
+    v-for="dialog in dialogs"
+    :key="dialog.id"
+    :model="dialog"
+    @close-dialog="handleDialogCancelBtn"
+  />
 </template>
 
 <style scoped></style>
