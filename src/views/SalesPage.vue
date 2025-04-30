33a19c90e648434ba1a1ec97fe264431cca597c4
sangcheol <tkdcjf38@iabacus.co.kr>
2025-04-30 13:30:38 +0900

    fix: 부서 범위 -> 부서 이름

--- src/views/SalesPage.vue
+++ src/views/SalesPage.vue
@@ -1,6 +1,12 @@
 <template>
   <v-col class="mt-3">
-    <SearchBarComponent :rows="searchRows" :immediateCheckboxEmit="true" @search="handleSearch" @reset="handleReset" :key="resetKey"/>
+    <SearchBarComponent
+      :rows="searchRows"
+      :immediateCheckboxEmit="true"
+      @search="handleSearch"
+      @reset="handleReset"
+      :key="resetKey"
+    />
   </v-col>
   <v-col>
     <TableComponent
@@ -24,13 +30,13 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue'
+import { computed, ref } from 'vue'
 import SearchBarComponent from '@/components/searchbar/SearchBarComponent.vue'
-import TableComponent from "@/components/table/TableComponent.vue"
-import ExcelActionsComponent from "@/components/common/ExcelActionsComponent.vue"
-import {getAggregate, downloadAggregate} from "@/apis/salesService.js";
-import {useRouter} from "vue-router";
-import { formatPrice } from '@/utils/MoneyUtils.js';
+import TableComponent from '@/components/table/TableComponent.vue'
+import ExcelActionsComponent from '@/components/common/ExcelActionsComponent.vue'
+import { downloadAggregate, getAggregate } from '@/apis/salesService.js'
+import { useRouter } from 'vue-router'
+import { formatPrice } from '@/utils/MoneyUtils.js'
 import { useToast } from 'vue-toastification'
 
 const resetKey = ref(0)
@@ -82,15 +88,13 @@ const searchRows = ref([
         key: 'dateRangeType',
         label: '적용 기간',
         type: 'select',
-        options: [
-          { title: '연도별', value: 'year' }
-        ],
+        options: [{ title: '연도별', value: 'year' }],
         md: 3,
       },
       {
         key: 'year',
         type: 'year',
-        md: 2
+        md: 2,
       },
     ],
   },
@@ -118,7 +122,7 @@ const searchRows = ref([
         value: false,
         md: 3,
       },
-    ]
+    ],
   },
 ])
 
@@ -137,7 +141,7 @@ const personnelCost = (num, count) => {
 // handleSearch: SearchBarComponent에서 필터가 전달되면 저장
 const handleSearch = async (filters) => {
   // 기존 params 업데이트
-  params.value = {...filters, page: 1}
+  params.value = { ...filters, page: 1 }
   currentPage.value = 1
   // 체크박스 등 검색 필터 저장
   searchFilters.value = { ...filters }
@@ -220,40 +224,165 @@ const dynamicHeaders = computed(() => {
   const headers = []
 
   // 항상 표시되는 기본 컬럼
-  headers.push({title: '부서범위', key: '부서범위', nowrap: true})
-  headers.push({title: '매출합계', key: '매출합계', nowrap: true, value: item => formatNumber(item['매출합계'])})
-  headers.push({title: '매출목표', key: '매출목표', nowrap: true, value: item => formatNumber(item['매출목표'])})
-  headers.push({title: '달성률', key: '달성률', nowrap: true, value: item => `${item['달성률']}%`})
-  headers.push({title: '영업이익', key: '영업이익', nowrap: true, value: item => formatNumber(item['영업이익'])})
-  headers.push({title: '영업이익률', key: '영업이익률', nowrap: true, value: item => `${item['영업이익률']}%`})
-  headers.push({title: '판관비', key: '판관비', nowrap: true, value: item => formatNumber(item['판관비'])})
-  headers.push({title: '제경비', key: '제경비', nowrap: true, value: item => formatNumber(item['제경비'])})
+  headers.push({ title: '부서이름', key: '부서이름', nowrap: true })
+  headers.push({
+    title: '매출합계',
+    key: '매출합계',
+    nowrap: true,
+    value: (item) => formatNumber(item['매출합계']),
+  })
+  headers.push({
+    title: '매출목표',
+    key: '매출목표',
+    nowrap: true,
+    value: (item) => formatNumber(item['매출목표']),
+  })
+  headers.push({
+    title: '달성률',
+    key: '달성률',
+    nowrap: true,
+    value: (item) => `${item['달성률']}%`,
+  })
+  headers.push({
+    title: '영업이익',
+    key: '영업이익',
+    nowrap: true,
+    value: (item) => formatNumber(item['영업이익']),
+  })
+  headers.push({
+    title: '영업이익률',
+    key: '영업이익률',
+    nowrap: true,
+    value: (item) => `${item['영업이익률']}%`,
+  })
+  headers.push({
+    title: '판관비',
+    key: '판관비',
+    nowrap: true,
+    value: (item) => formatNumber(item['판관비']),
+  })
+  headers.push({
+    title: '제경비',
+    key: '제경비',
+    nowrap: true,
+    value: (item) => formatNumber(item['제경비']),
+  })
 
   // 추가 정보에 따른 선택적 컬럼
   if (searchFilters.value.checkTypeWage) {
-    headers.push({title: '정직원 인건비(인원)', key: '정직원', nowrap: true, value: item => personnelCost((item['정직원인건비']), formatNumber(item['정직원']))})
-    headers.push({title: '프리랜서 인건비(인원)', key: '프리랜서', nowrap: true, value: item => personnelCost((item['프리랜서인건비']), formatNumber(item['프리랜서']))})
-    headers.push({title: '외주 인건비(인원)', key: '외주', nowrap: true, value: item => personnelCost((item['외주인건비']), formatNumber(item['외주']))})
-  }else{
-    headers.push({title: '인건비', key: '인건비', nowrap: true, value: item => formatNumber(item['인건비'])})
+    headers.push({
+      title: '정직원 인건비(인원)',
+      key: '정직원',
+      nowrap: true,
+      value: (item) => personnelCost(item['정직원인건비'], formatNumber(item['정직원'])),
+    })
+    headers.push({
+      title: '프리랜서 인건비(인원)',
+      key: '프리랜서',
+      nowrap: true,
+      value: (item) => personnelCost(item['프리랜서인건비'], formatNumber(item['프리랜서'])),
+    })
+    headers.push({
+      title: '외주 인건비(인원)',
+      key: '외주',
+      nowrap: true,
+      value: (item) => personnelCost(item['외주인건비'], formatNumber(item['외주'])),
+    })
+  } else {
+    headers.push({
+      title: '인건비',
+      key: '인건비',
+      nowrap: true,
+      value: (item) => formatNumber(item['인건비']),
+    })
   }
   if (searchFilters.value.checkTypeAmount) {
-    headers.push({title: 'SI', key: 'si', nowrap: true, value: item => formatNumber(item['si'])})
-    headers.push({title: 'SM', key: 'sm', nowrap: true, value: item => formatNumber(item['sm'])})
+    headers.push({
+      title: 'SI',
+      key: 'si',
+      nowrap: true,
+      value: (item) => formatNumber(item['si']),
+    })
+    headers.push({
+      title: 'SM',
+      key: 'sm',
+      nowrap: true,
+      value: (item) => formatNumber(item['sm']),
+    })
   }
   if (searchFilters.value.checkMonthlyAmount) {
-    headers.push({title: '1월', key: 'sales_01', nowrap: true, value: item => formatNumber(item['sales_01'])})
-    headers.push({title: '2월', key: 'sales_02', nowrap: true, value: item => formatNumber(item['sales_02'])})
-    headers.push({title: '3월', key: 'sales_03', nowrap: true, value: item => formatNumber(item['sales_03'])})
-    headers.push({title: '4월', key: 'sales_04', nowrap: true, value: item => formatNumber(item['sales_04'])})
-    headers.push({title: '5월', key: 'sales_05', nowrap: true, value: item => formatNumber(item['sales_05'])})
-    headers.push({title: '6월', key: 'sales_06', nowrap: true, value: item => formatNumber(item['sales_06'])})
-    headers.push({title: '7월', key: 'sales_07', nowrap: true, value: item => formatNumber(item['sales_07'])})
-    headers.push({title: '8월', key: 'sales_08', nowrap: true, value: item => formatNumber(item['sales_08'])})
-    headers.push({title: '9월', key: 'sales_09', nowrap: true, value: item => formatNumber(item['sales_09'])})
-    headers.push({title: '10월', key: 'sales_10', nowrap: true, value: item => formatNumber(item['sales_10'])})
-    headers.push({title: '11월', key: 'sales_11', nowrap: true, value: item => formatNumber(item['sales_11'])})
-    headers.push({title: '12월', key: 'sales_12', nowrap: true, value: item => formatNumber(item['sales_12'])})
+    headers.push({
+      title: '1월',
+      key: 'sales_01',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_01']),
+    })
+    headers.push({
+      title: '2월',
+      key: 'sales_02',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_02']),
+    })
+    headers.push({
+      title: '3월',
+      key: 'sales_03',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_03']),
+    })
+    headers.push({
+      title: '4월',
+      key: 'sales_04',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_04']),
+    })
+    headers.push({
+      title: '5월',
+      key: 'sales_05',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_05']),
+    })
+    headers.push({
+      title: '6월',
+      key: 'sales_06',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_06']),
+    })
+    headers.push({
+      title: '7월',
+      key: 'sales_07',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_07']),
+    })
+    headers.push({
+      title: '8월',
+      key: 'sales_08',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_08']),
+    })
+    headers.push({
+      title: '9월',
+      key: 'sales_09',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_09']),
+    })
+    headers.push({
+      title: '10월',
+      key: 'sales_10',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_10']),
+    })
+    headers.push({
+      title: '11월',
+      key: 'sales_11',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_11']),
+    })
+    headers.push({
+      title: '12월',
+      key: 'sales_12',
+      nowrap: true,
+      value: (item) => formatNumber(item['sales_12']),
+    })
   }
 
   return headers
